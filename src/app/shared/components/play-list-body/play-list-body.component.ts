04fda59e094db90dbe5d4c684659edcdab3d3372
abelsaul <abelmiraval@gmail.com>
2022-04-26 06:19:07 -0500

    Add shared play-list-body

---
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../data/tracks.json'
+
+@Component({
+  selector: 'app-play-list-body',
+  templateUrl: './play-list-body.component.html',
+  styleUrls: ['./play-list-body.component.css']
+})
+export class PlayListBodyComponent implements OnInit {
+  tracks: TrackModel[] = []
+  constructor() { }
+
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.tracks = data;
+  }
+
+}
