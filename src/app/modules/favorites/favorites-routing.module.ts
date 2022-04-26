16aa61131964a1ffb0cc408a5faefdc71c754ea4
abelsaul <abelmiraval@gmail.com>
2022-04-26 06:22:37 -0500

    Update module favorites

--- src/app/modules/favorites/favorites-routing.module.ts
+++ src/app/modules/favorites/favorites-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritePageComponent } from './page/favorite-page/favorite-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FavoritePageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/favorites/favorites.module.ts
+++ src/app/modules/favorites/favorites.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FavoritesRoutingModule } from './favorites-routing.module';
 import { FavoritePageComponent } from './page/favorite-page/favorite-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { FavoritePageComponent } from './page/favorite-page/favorite-page.compon
   ],
   imports: [
     CommonModule,
-    FavoritesRoutingModule
+    FavoritesRoutingModule,
+    SharedModule
   ]
 })
 export class FavoritesModule { }
