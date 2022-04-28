58128d02e9360b98896a8edf7d5d8a85b3490d06
abelsaul <abelmiraval@gmail.com>
2022-04-28 08:57:30 -0500

    Add directive img-broken

---
+++ src/app/shared/directives/img-broken.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appImgBroken]'
+})
+export class ImgBrokenDirective {
+  @Input() customImg: string = ''
+  @HostListener('error') handleError(): void {
+    const elNative = this.elHost.nativeElement
+    console.log(' 🔴 Esta imagen revento -->', this.elHost);
+    elNative.src = '../../../assets/images/image-broken.png'
+    elNative.src = this.customImg
+  }
+
+  constructor(private elHost: ElementRef) {
+    console.log(this.elHost)
+  }
+
+}
