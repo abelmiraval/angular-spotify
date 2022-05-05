355630009d0aca4361ec7b11cd2132136efdb1f6
abelsaul <abelmiraval@gmail.com>
2022-05-05 11:52:31 -0500

    Add guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionGuard } from '@core/guards/session.guard';
 import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [// TODO router-outlet(Padre)
@@ -8,9 +9,10 @@ const routes: Routes = [// TODO router-outlet(Padre)
     loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
   },
   {
-    path: '',//TODO (Private)
+    path: '',//TODO (Private) 🔴🔴
     component: HomePageComponent,
-    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
+    loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule),
+    canActivate: [SessionGuard]
   }
 ];
 
