9be53029b3203ea0268d437f9a166982050efd17
abelsaul <abelmiraval@gmail.com>
2022-05-06 08:29:18 -0500

    Add search service

---
+++ src/app/modules/history/services/search.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+  private readonly URL = environment.api
+
+  constructor(private http: HttpClient) { }
+
+  searchTracks$(term: string): Observable<any> {
+    return this.http.get(`${this.URL}/tracks?src=${term}`)
+  }
+
+}
