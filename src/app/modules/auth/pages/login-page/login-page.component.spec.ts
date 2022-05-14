e47a0bb6cd36d20b07b8d99766587c74fcb0a42f
abelsaul <abelmiraval@gmail.com>
2022-05-14 13:41:50 -0500

    Add components - test

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginPageComponent } from './login-page.component';
@@ -22,7 +23,58 @@ describe('LoginPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  //TODO: Tu primer enunciado el cual debe asegurar lo siguiente
+  //TODO: Debe de asegurarse que el formulario sea invalido cuando ingrese dato erroneos
+
+  //TODO: Patron AAA
+
+  it('Deberia de retornar "invalido" el formulario', () => {
+
+    //TODO: Arrange
+    const mockCredentials = {
+      email: '0x0x0x0x0x0',
+      password: '11111111111111111111111111111'
+    }
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    //TODO: Act
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    //TODO: Assert
+
+    expect(component.formLogin.invalid).toEqual(true)
+  });
+
+  it('Deberia de retornar "valido" el formulario', () => {
+
+    //TODO: Arrange
+    const mockCredentials = {
+      email: 'test@test.com',
+      password: '12345678'
+    }
+
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    //TODO: Act
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    //TODO: Assert
+
+    expect(component.formLogin.invalid).toEqual(false)
+  });
+
+  it('El boton deberia de tener la palabra "Iniciar sesión"', () => {
+
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'))
+    const getInnerText = elementRef.nativeElement.innerText
+
+    expect(getInnerText).toEqual('Iniciar sesión')
   });
 });
