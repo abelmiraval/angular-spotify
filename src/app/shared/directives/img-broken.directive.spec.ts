519314c2b4a6f444762d0d7cbce9abfe4874c554
abelsaul <abelmiraval@gmail.com>
2022-05-17 16:08:56 -0500

    Add directive - test

--- src/app/shared/directives/img-broken.directive.spec.ts
+++ src/app/shared/directives/img-broken.directive.spec.ts
@@ -1,9 +1,37 @@
-import { ElementRef } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ImgBrokenDirective } from './img-broken.directive';
+
+//TODO: Componente de prueba
+@Component({
+  template: '<img class="testing-directive" appImgBroken [src]="srcMock">'
+})
+
+class TestComponent {
+  public srcMock: any = null
+}
 
 //TODO La prueba de ImgBroken es la siguiente
 
 describe('ImgBrokenDirective', () => {
+
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        ImgBrokenDirective
+      ]
+    })
+
+    fixture = TestBed.createComponent(TestComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+
+  })
 
   //TODO Deberia instanciar correctamente
   it('should create an instance', () => {
@@ -11,4 +39,24 @@ describe('ImgBrokenDirective', () => {
     const directive = new ImgBrokenDirective(mockElement);
     expect(directive).toBeTruthy();
   });
+
+  it('🤨🤨  TestComponent deberia instanciarse correcatmente', () => {
+    expect(component).toBeTruthy()
+  })
+
+  it('😁🤨 Directiva deberia de cambiar la imagen por un base64', (done: DoneFn) => {
+    //TODO: Arrange
+    const beforeImgElement = fixture.debugElement.query(By.css('.testing-directive')).nativeElement
+    const beforeImgSrc = beforeImgElement.src // TODO 👉 Tenemos la url antes de ser cambiada por la directiva
+    component.srcMock = undefined
+
+    setTimeout(() => {
+      const afterImgElement = fixture.debugElement.query(By.css('.testing-directive')).nativeElement
+      const afterImgSrc = beforeImgElement.src //TODO 👉 Tenemos la url despues de ser cambiada por la directiva
+      expect(afterImgSrc).toMatch(/\bdata:image\b/)//TODO data:image10100101010
+      done()
+    }, 3000)
+
+  })
+
 });
--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -4,12 +4,16 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
   selector: '[appImgBroken]'
 })
 export class ImgBrokenDirective {
-  @Input() customImg: string = ''
+  @Input() customImg: string | boolean = false
   @HostListener('error') handleError(): void {
     const elNative = this.elHost.nativeElement
     console.log(' 🔴 Esta imagen revento -->', this.elHost);
-    elNative.src = '../../../assets/images/image-broken.png'
-    elNative.src = this.customImg
+    if (this.customImg) {
+      elNative.src = this.customImg
+    } else {
+      // elNative.src = '/assets/images/image-broken.png'
+      elNative.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAAXNSR0IArs4c6QAAAD5JREFUGFdtjKENAEAIA4vC49h/HWbAERwexYfXVDW9S8nMRkTAzOhuVBXI3WeLqiIzsQJFxAD4w4LNbV6fD32XLKyP2o8zAAAAAElFTkSuQmCC'
+    }
   }
 
   constructor(private elHost: ElementRef) {
